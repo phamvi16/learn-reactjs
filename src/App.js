34a95211a497195373e8dc879791b4ce825b028f
phamvi16 <phamthuyvi2710@gmail.com>
2020-11-05 11:06:20 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Vi pham
+         Pham Thuy Vi
         </p>
         <a
           className="App-link"
